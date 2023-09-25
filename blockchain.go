be8ddfc0df0b1178b5850f80e1d4e94bdd85c712
scarfacedotcom <peteradaaku@gmail.com>
2023-09-25 05:21:43 +0100

    first commit

---
+++ blockchain.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	prevHash     string
+	timestamp    int64
+	transactions []string
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp  %d\n", b.timestamp)
+	fmt.Printf("nonce  %d\n", b.nonce)
+	fmt.Printf("prevvoius_hash  %s\n", b.prevHash)
+	fmt.Printf("transactions  %s\n", b.transactions)
+
+}
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreatedBlock(0, "Init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreatedBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func NewBlock(nonce int, prevHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.prevHash = prevHash
+	return b
+}
+
+func main() {
+	b := NewBlock(0, "init Hash")
+	b.Print()
+}
